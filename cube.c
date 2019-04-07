797c69f6ba09655d0574e00b8781893ce7f0bb8e
Arnold Lestin <amlestin@gmail.com>
2019-04-07 18:38:55 -0400

    Removed fight semaphores

--- cube.c
+++ cube.c
@@ -630,7 +630,7 @@ int fight_wizard(struct wizard *self, struct wizard *other, struct room *room)
 
     /* Fill in */
     other->status = 1;
-    sem_wait(&other->wizard_status);
+//    sem_wait(&other->wizard_status);
   }
 
   /* Self freezes and release the lock */
@@ -643,7 +643,7 @@ int fight_wizard(struct wizard *self, struct wizard *other, struct room *room)
 
     /* Fill in */
     self->status = 1;
-    sem_wait(&self->wizard_status);
+//    sem_wait(&self->wizard_status);
     return 1;
   }
   int winner_flag = check_winner(self->cube);
